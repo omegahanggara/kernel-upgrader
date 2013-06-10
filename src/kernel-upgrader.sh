5f52277708ca42a88d3b5f07fab75ae4003b54fb
Omega Hanggara <hanggara33@gmail.com>
2013-06-10 15:24:32 +0700

    Fixed color to red on cout warning

--- src/kernel-upgrader.sh
+++ src/kernel-upgrader.sh
@@ -11,7 +11,7 @@ linuxType=$(lsb_release --short --id)
 cin() {
 	if [ "$1" == "action" ] ; then output="\e[01;32m[>]\e[00m" ; fi
 	if [ "$1" == "info" ] ; then output="\e[01;33m[i]\e[00m" ; fi
-	if [ "$1" == "warning" ] ; then output="\e[01;32m[w]\e[00m" ; fi
+	if [ "$1" == "warning" ] ; then output="\e[01;31m[w]\e[00m" ; fi
 	if [ "$1" == "error" ] ; then output="\e[01;31m[e]\e[00m" ; fi
 	output="$output $2"
 	echo -en "$output"
@@ -20,7 +20,7 @@ cin() {
 cout() {
 	if [ "$1" == "action" ] ; then output="\e[01;32m[>]\e[00m" ; fi
 	if [ "$1" == "info" ] ; then output="\e[01;33m[i]\e[00m" ; fi
-	if [ "$1" == "warning" ] ; then output="\e[01;32m[w]\e[00m" ; fi
+	if [ "$1" == "warning" ] ; then output="\e[01;31m[w]\e[00m" ; fi
 	if [ "$1" == "error" ] ; then output="\e[01;31m[e]\e[00m" ; fi
 	output="$output $2"
 	echo -e "$output"
